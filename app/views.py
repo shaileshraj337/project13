64d90993d103d1c8d74cd69b8d00f1d3f26d4381
SHAILESH RAJ <75255626+shaileshraj337@users.noreply.github.com>
2022-03-31 23:28:55 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def cdn_bootstrap(request):
+    return render(request,'cdn_bootstrap.html')
+
+
+def local_bootstrap(request):
+    return render(request,'local_bootstrap.html')
